03960fb4bd07586f5c2285a47eaaa46955d73335
alicemusial <alicjamusial3@gmail.com>
2023-12-29 23:21:28 +0100

    login page

---
+++ front/src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import { useForm } from '@mantine/form'
+import {
+    TextInput,
+    PasswordInput,
+    Group,
+    Button,
+    Stack,
+    Container,
+  } from '@mantine/core';
+
+const Login = () => {
+    const form = useForm({
+      initialValues: {
+        username: '',
+        password: '',
+        terms: true,
+      },
+  
+      validate: {
+        email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+        password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
+      },
+    });
+    return (
+        <Container size={420} my={40}>
+        <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+
+
+        <TextInput
+            required
+            label="Username"
+            placeholder="Joe"
+            value={form.values.username}
+            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            error={form.errors.email && 'Invalid email'}
+            radius="md"
+        />
+
+        <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={form.values.password}
+            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+            error={form.errors.password && 'Password should include at least 6 characters'}
+            radius="md"
+        />
+
+        
+        </Stack>
+
+        <Group justify="flex-end" mt="xl">
+
+        <Button type="submit" radius="xl">
+            Login
+        </Button>
+        </Group>
+    </form>
+    </Container>)
+};
+
+export default Login;
