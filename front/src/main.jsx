e63a89070b259dddc3190d01a7021aa3bcb97eb5
alicemusial <alicjamusial3@gmail.com>
2023-12-29 20:03:31 +0100

    frontend setup

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={{
+      primaryColor: 'BlueGray',
+      colors: {
+      'BlueGray': [
+        '#f3f4f7',
+        '#e5e5e7',
+        '#c6c8ce',
+        '#a7aab7',
+        '#8c90a2',
+        '#7b8096',
+        '#717792',
+        '#60667f',
+        '#555a72',
+        '#474e66'
+      ]
+    }}}>
+     <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
